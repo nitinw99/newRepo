20e9a103f5c26015bdff7af919985c99aee45eec
nitinw99 <98516195+nitinw99@users.noreply.github.com>
2022-02-02 00:42:54 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,23 @@
+my_message = "Wassup Nitin"
+
+# print(my_message.lower())
+
+# print(my_message.upper())
+
+# print(my_message.count('n'))
+
+# print(my_message.find('Nit'))
+
+# print(my_message)
+
+# my_message = my_message.replace('Wassup', 'Heyy')
+
+# print(my_message)
+
+message2 = "How are ya"
+
+# my_message = my_message + '. ' + message2 + '? Welcome!'
+# print(my_message)
+
+my_message = '{}. {}? Welcome'.format(my_message, message2)
+print(my_message)
